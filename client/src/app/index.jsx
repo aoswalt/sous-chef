ea412c759f11e6c5fa7926aa80cfbbac72ce4461
Adam Oswalt <email@adamoswalt.com>
2016-12-11 16:40:18 -0600

    react uses "classname"

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -3,41 +3,41 @@ import ReactDOM from 'react-dom'
 
 ReactDOM.render(
   (<div>
-    <h1 class="recipe-title">Recipe Title</h1>
+    <h1 className="recipe-title">Recipe Title</h1>
 
-    <span class="servings">2 Servings</span>
+    <span className="servings">2 Servings</span>
 
-    <div class="ingredients">
+    <div className="ingredients">
       <h2>Ingredients</h2>
-      <ul class="ingredient-list">
+      <ul className="ingredient-list">
         <li>1 Tbsp butter</li>
         <li>2 oz cream</li>
         <li>1/4 cup flour</li>
       </ul>
     </div>
 
-    <div class="time">
-      <span class="total-time">20 minutes</span>
-      <span class="prep-time">5 minutes</span>
-      <span class="wait-time">0 minutes</span>
-      <span class="cook-time">15 minutes</span>
+    <div className="time">
+      <span className="total-time">20 minutes</span>
+      <span className="prep-time">5 minutes</span>
+      <span className="wait-time">0 minutes</span>
+      <span className="cook-time">15 minutes</span>
     </div>
 
-    <div class="steps">
+    <div className="steps">
       <h2>Steps</h2>
-      <ol class="step-list">
+      <ol className="step-list">
         <li>Melt butter in pan.</li>
         <li>Whisk in flour.</li>
         <li>Add cream.</li>
       </ol>
     </div>
 
-    <div class="tags">
+    <div className="tags">
       <h3>Tags</h3>
-      <ul class="tag-list">
-        <li class="tag">base</li>
-        <li class="tag">sauce</li>
-        <li class="tag">simple</li>
+      <ul className="tag-list">
+        <li className="tag">base</li>
+        <li className="tag">sauce</li>
+        <li className="tag">simple</li>
       </ul>
     </div>
   </div>),
@@ -46,16 +46,16 @@ ReactDOM.render(
 
 ReactDOM.render(
   (<div>
-      <div class="title-pane">
+      <div className="title-pane">
         <h1>Sous Chef</h1>
       </div>
 
-      <ul class="menu">
+      <ul className="menu">
         <li>Recipe Box</li>
-        <li class="separator"></li>
+        <li className="separator"></li>
         <li>Add To List</li>
         <li>Generate List</li>
-        <li class="separator"></li>
+        <li className="separator"></li>
         <li>Find Recipes</li>
       </ul>
   </div>),
