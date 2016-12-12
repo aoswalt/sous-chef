2c87bb86a86f5012503535dc5782043d5934c8cf
Adam Oswalt <email@adamoswalt.com>
2016-12-11 21:12:18 -0600

    unify to single quotes

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -29,16 +29,16 @@ ReactDOM.render(
 
 ReactDOM.render(
   (<div>
-      <div className="title-pane">
+      <div className='title-pane'>
         <h1>Sous Chef</h1>
       </div>
 
-      <ul className="menu">
+      <ul className='menu'>
         <li>Recipe Box</li>
-        <li className="separator"></li>
+        <li className='separator'></li>
         <li>Add To List</li>
         <li>Generate List</li>
-        <li className="separator"></li>
+        <li className='separator'></li>
         <li>Find Recipes</li>
       </ul>
   </div>),
--- client/src/app/recipe/Recipe.jsx
+++ client/src/app/recipe/Recipe.jsx
@@ -2,36 +2,36 @@ import React from 'react'
 
 const Recipe = (props) =>
   (<div>
-    <h1 className="recipe-title">{props.title}</h1>
+    <h1 className='recipe-title'>{props.title}</h1>
 
-    <span className="servings">{props.servings} Servings</span>
+    <span className='servings'>{props.servings} Servings</span>
 
-    <div className="ingredients">
+    <div className='ingredients'>
       <h2>Ingredients</h2>
-      <ul className="ingredient-list">
+      <ul className='ingredient-list'>
         {props.ingredients.map((ing, ind) => <li key={ind}>{ing}</li>)}
       </ul>
     </div>
 
-    <div className="time">
-      <span className="total-time">Total: {props.times.total} minutes</span>
-      {props.times.prep? <span className="prep-time">Prep: {props.times.prep} minutes</span> : ''}
-      {props.times.wait? <span className="wait-time">Wait: {props.times.wait} minutes</span> : ''}
-      {props.times.cook? <span className="cook-time">Cook: {props.times.cook} minutes</span> : ''}
+    <div className='time'>
+      <span className='total-time'>Total: {props.times.total} minutes</span>
+      {props.times.prep? <span className='prep-time'>Prep: {props.times.prep} minutes</span> : ''}
+      {props.times.wait? <span className='wait-time'>Wait: {props.times.wait} minutes</span> : ''}
+      {props.times.cook? <span className='cook-time'>Cook: {props.times.cook} minutes</span> : ''}
     </div>
 
-    <div className="steps">
+    <div className='steps'>
       <h2>Steps</h2>
-      <ol className="step-list">
-        {props.steps.map((s, i) => <li className="step" key={i}>{s}</li>)}
+      <ol className='step-list'>
+        {props.steps.map((s, i) => <li className='step' key={i}>{s}</li>)}
       </ol>
     </div>
 
     {(props.tags) ?
-      <div className="tags">
+      <div className='tags'>
         <h3>Tags</h3>
-        <ul className="tag-list">
-          {props.tags.map((t) => <li className="tag" key={t}>{t}</li>)}
+        <ul className='tag-list'>
+          {props.tags.map((t) => <li className='tag' key={t}>{t}</li>)}
         </ul>
       </div>
     : ''}
