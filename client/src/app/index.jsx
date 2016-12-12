6763a8963862b227d4d3b06ae2fa22353626c194
Adam Oswalt <email@adamoswalt.com>
2016-12-11 20:59:31 -0600

    add props to recipe component

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -3,7 +3,27 @@ import ReactDOM from 'react-dom'
 import Recipe from './recipe/Recipe.jsx'
 
 ReactDOM.render(
-  <Recipe />,
+  <Recipe
+    title='Tasty Sauce'
+    servings={2}
+    ingredients={[
+        '1 Tbsp butter',
+        '2 oz cream',
+        '1/4 cup flour',
+    ]}
+    times={{
+      total: 20,
+      prep: 5,
+      //wait: '',
+      cook: 15,
+    }}
+    steps={[
+        'Melt butter in pan.',
+        'Whisk in flour.',
+        'Add cream.',
+    ]}
+    tags={['sauce', 'simple']}
+  />,
   document.querySelector('.main')
 )
 
--- client/src/app/recipe/Recipe.jsx
+++ client/src/app/recipe/Recipe.jsx
@@ -1,44 +1,49 @@
 import React from 'react'
 
 const Recipe = (props) =>
-  <div>
-    <h1 className="recipe-title">Recipe Title</h1>
+  (<div>
+    <h1 className="recipe-title">{props.title}</h1>
 
-    <span className="servings">2 Servings</span>
+    <span className="servings">{props.servings} Servings</span>
 
     <div className="ingredients">
       <h2>Ingredients</h2>
       <ul className="ingredient-list">
-        <li>1 Tbsp butter</li>
-        <li>2 oz cream</li>
-        <li>1/4 cup flour</li>
+        {props.ingredients.map((ing, ind) => <li key={ind}>{ing}</li>)}
       </ul>
     </div>
 
     <div className="time">
-      <span className="total-time">20 minutes</span>
-      <span className="prep-time">5 minutes</span>
-      <span className="wait-time">0 minutes</span>
-      <span className="cook-time">15 minutes</span>
+      <span className="total-time">Total: {props.times.total} minutes</span>
+      {props.times.prep? <span className="prep-time">Prep: {props.times.prep} minutes</span> : ''}
+      {props.times.wait? <span className="wait-time">Wait: {props.times.wait} minutes</span> : ''}
+      {props.times.cook? <span className="cook-time">Cook: {props.times.cook} minutes</span> : ''}
     </div>
 
     <div className="steps">
       <h2>Steps</h2>
       <ol className="step-list">
-        <li>Melt butter in pan.</li>
-        <li>Whisk in flour.</li>
-        <li>Add cream.</li>
+        {props.steps.map((s, i) => <li className="step" key={i}>{s}</li>)}
       </ol>
     </div>
 
-    <div className="tags">
-      <h3>Tags</h3>
-      <ul className="tag-list">
-        <li className="tag">base</li>
-        <li className="tag">sauce</li>
-        <li className="tag">simple</li>
-      </ul>
-    </div>
-  </div>
+    {(props.tags) ?
+      <div className="tags">
+        <h3>Tags</h3>
+        <ul className="tag-list">
+          {props.tags.map((t) => <li className="tag" key={t}>{t}</li>)}
+        </ul>
+      </div>
+    : ''}
+  </div>)
+
+Recipe.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  servings: React.PropTypes.number.isRequired,
+  ingredients: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  times: React.PropTypes.objectOf(React.PropTypes.number).isRequired,
+  steps: React.PropTypes.arrayOf(React.PropTypes.string),
+  tags: React.PropTypes.arrayOf(React.PropTypes.string)
+}
 
 export default Recipe
